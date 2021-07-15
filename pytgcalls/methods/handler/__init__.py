39c62bd28dff3ce8a45ae0046ea00e73a681d1e5
BlackStone <79520370+BlackStoneReborn@users.noreply.github.com>
2021-07-15 13:58:06 +0200

    Revert "x -1"
    This reverts commit 724e451aa7bb1404ad96114724bb3003e6dec0a8.

--- pytgcalls/methods/handler/__init__.py
+++ pytgcalls/methods/handler/__init__.py
@@ -1,7 +1,6 @@
 from .on_closed_voice_chat import OnClosedVoiceChat
 from .on_group_call_invite import OnGroupCallInvite
 from .on_kicked import OnKicked
-from .on_no_voice_chat import OnNoVoiceChat
 from .on_raw_event import OnRawUpdate
 from .on_stream_end import OnStreamEnd
 
@@ -10,7 +9,6 @@ class Handler(
     OnClosedVoiceChat,
     OnGroupCallInvite,
     OnKicked,
-    OnNoVoiceChat,
     OnRawUpdate,
     OnStreamEnd,
 ):
